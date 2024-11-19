cfee3ae2ad0b3132773fd51430b43793d51d2642
xinyuejianke <100462582+xinyuejianke@users.noreply.github.com>
2024-11-19 21:21:44 +0800

    chapter 12 add update action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,7 +5,7 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod'
 
-const FromSchema = z.object({
+const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
   amount: z.coerce.number(),
@@ -13,13 +13,13 @@ const FromSchema = z.object({
   date: z.string()
 })
 
-const CreateInvoice = FromSchema.omit({ id: true, date: true });
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(fromData: FormData) {
+export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: fromData.get('customerId'),
-    amount: fromData.get('amount'),
-    status: fromData.get('status')
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
@@ -27,6 +27,26 @@ export async function createInvoice(fromData: FormData) {
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date) 
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date});`
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql` UPDATE invoices 
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}, date = ${date}
+    WHERE id=${id};`
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
