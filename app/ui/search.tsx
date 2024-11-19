b5b158761509b0398efebec488bcda3f9cbc58c5
xinyuejianke <100462582+xinyuejianke@users.noreply.github.com>
2024-11-19 17:15:40 +0800

    chapter 12 add invoice

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,7 +13,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching...${term}`);
     const params = new URLSearchParams(searchParam);
-    params.set('page', 1);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
